f12b4dc3bf8ab5cf354c90934f8fe928e105869a
Leon Patrick Mawlong <leonmawlong@gmail.com>
2019-02-12 07:35:22 +0530

    updated Makefile

--- heap/heapPrint.cpp
+++ heap/heapPrint.cpp
@@ -4,7 +4,7 @@ using namespace std;
 void heapPrint(int *a, int n)
 {
     int i;
-    cout<<"\nHeap: \n";
+    cout<<"\nMax Heap: \n";
     for (i = 1; i <= n; i++)
         cout<<a[i]<<endl;    
 }
